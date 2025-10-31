e01db2405c943e1cacbbd86ab7295105db25ddcc
Nam-Bao <nguyenxuannhatnam8@gmail.com>
2025-10-31 16:22:25 +0700

    return

--- auth/src/config/index.js
+++ auth/src/config/index.js
@@ -6,13 +6,3 @@ module.exports = {
 };
 
 
-// require("dotenv").config();
-
-// module.exports = {
-//   // Ưu tiên URI test từ mongodb-memory-server nếu có
-//   mongoURI:
-//     process.env.MONGO_URI || // dùng cho test
-//     process.env.MONGODB_AUTH_URI || // dùng cho môi trường thực
-//     "mongodb://127.0.0.1:27017/auth_service", // fallback local
-//   jwtSecret: process.env.JWT_SECRET || "secret",
-// };
